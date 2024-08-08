3b16e51611784bb1dfe9d02e7f379281b46da285
Amankhl <khowalaman93@gmail.com>
2024-08-08 18:05:55 +0530

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/header/Header";
+import Header from "@/components/header/Header";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         {children}
         </body>
     </html>
